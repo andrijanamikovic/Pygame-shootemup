652d48b9d099c1e66c398e721c864e20a38ae748
Andrijana Mikovic <andrijana.mikovic00@gmail.com>
2023-03-25 01:56:21 +0100

    Menu after every level

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pygame_menu
 import os
 import time
 import random
+
 pygame.font.init()
 
-WIDTH, HEIGHT = 750, 750
+WIDTH, HEIGHT = 900, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Grogu")
 pygame.init()
@@ -30,6 +31,9 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+menu_theme = pygame_menu.themes.THEME_DARK
+menu = pygame_menu.Menu("Select a weapon", 300, 300, theme=menu_theme)
+
 
 class Laser:
     def __init__(self, x, y, img):
@@ -45,7 +49,7 @@ class Laser:
         self.y += vel
 
     def off_screen(self, height):
-        return not(self.y <= height and self.y >= 0)
+        return not (self.y <= height and self.y >= 0)
 
     def collision(self, obj):
         return collide(self, obj)
@@ -137,13 +141,17 @@ class Player(Ship):
                         if laser in self.lasers:
                             self.lasers.remove(laser)
 
-    def draw(self, window):
+    def draw(self, window, health):
         super().draw(window)
+        self.health = health
         self.healthbar(window)
 
     def healthbar(self, window):
-        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
-        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/self.max_health), 10))
+        pygame.draw.rect(window, (255, 0, 0),
+                         (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0, 255, 0), (
+        self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health / self.max_health),
+        10))
 
     def change_laser_color(self, laser_img_color):
         if laser_img_color == "Red":
@@ -157,13 +165,12 @@ class Player(Ship):
             self.laser = 3
 
 
-
 class Enemy(Ship):
     COLOR_MAP = {
-                "red": (RED_ENEMY, RED_LASER),
-                "black": (BLACK_ENEMY, GREEN_LASER),
-                "white": (WHITE_ENEMY, BLUE_LASER)
-                }
+        "red": (RED_ENEMY, RED_LASER),
+        "black": (BLACK_ENEMY, GREEN_LASER),
+        "white": (WHITE_ENEMY, BLUE_LASER)
+    }
 
     def __init__(self, x, y, color, health=100):
         if color == "red":
@@ -182,7 +189,7 @@ class Enemy(Ship):
 
     def shoot(self):
         if self.cool_down_counter == 0:
-            laser = Laser(self.x-20, self.y, self.laser_img)
+            laser = Laser(self.x - 20, self.y, self.laser_img)
             self.lasers.append(laser)
             self.cool_down_counter = 1
 
@@ -192,34 +199,61 @@ def collide(obj1, obj2):
     offset_y = obj2.y - obj1.y
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
-def main(selected_weapon):
+level = 0
+lives = 5
+enemies = []
+wave_length = 5
+enemy_vel = 1
+
+player_vel = 5
+laser_vel = 5
+player = Player(300, 630)
+
+lost = False
+lost_count = 0
+def main():
     run = True
     FPS = 90
-    level = 0
-    lives = 5
+    global init
+    global level
+    global lives
+    global enemies
+    global wave_length
+    global enemy_vel
+    global lost
+    global lost_count
+    global player_vel
+    global laser_vel
+    global player
+    if not init:
+        level = 0
+        lives = 5
+
+
+        enemies = []
+        wave_length = 5
+        enemy_vel = 1
+
+        player_vel = 5
+        laser_vel = 5
+
+        lost = False
+        lost_count = 0
+        init = True
     main_font = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 60)
-
-    enemies = []
-    wave_length = 5
-    enemy_vel = 1
-
-    player_vel = 5
-    laser_vel = 5
-
-    player = Player(300, 630)
-
     clock = pygame.time.Clock()
+    player.change_laser_color(weapon)
 
-    lost = False
-    lost_count = 0
-    player.change_laser_color(selected_weapon)
+    def choose_new_weapon():
+        menu.enable()
+        menu.mainloop(WIN)
 
     def redraw_window():
-        WIN.blit(BG, (0,0))
+        WIN.blit(BG, (0, 0))
         # draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
-        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
 
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
@@ -227,11 +261,11 @@ def main(selected_weapon):
         for enemy in enemies:
             enemy.draw(WIN)
 
-        player.draw(WIN)
+        player.draw(WIN, player.health)
 
         if lost:
-            lost_label = lost_font.render("You Lost!!", 1, (255,255,255))
-            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+            lost_label = lost_font.render("You Lost!!", 1, (255, 255, 255))
+            WIN.blit(lost_label, (WIDTH / 2 - lost_label.get_width() / 2, 350))
 
         pygame.display.update()
 
@@ -246,6 +280,8 @@ def main(selected_weapon):
         if lost:
             if lost_count > FPS * 3:
                 run = False
+                player.health = 100
+                init = False
             else:
                 continue
 
@@ -253,21 +289,26 @@ def main(selected_weapon):
             level += 1
             wave_length += 5
             for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "black", "white"]))
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100),
+                              random.choice(["red", "black", "white"]))
                 enemies.append(enemy)
+            if level != 1:
+                choose_new_weapon()
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
 
         keys = pygame.key.get_pressed()
-        if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and player.x - player_vel > 0: # left
+        if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and player.x - player_vel > 0:  # left
             player.x -= player_vel
-        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player.x + player_vel + player.get_width() < WIDTH: # right
+        if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and player.x + player_vel + player.get_width() < WIDTH:  # right
             player.x += player_vel
-        if (keys[pygame.K_w] or keys[pygame.K_UP]) and player.y - player_vel > 0: # up
+        if (keys[pygame.K_w] or keys[pygame.K_UP]) and player.y - player_vel > 0:  # up
             player.y -= player_vel
-        if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and player.y + player_vel + player.get_height() + 15 < HEIGHT: # down
+        if (keys[pygame.K_s] or keys[
+            pygame.K_DOWN]) and player.y + player_vel + player.get_height() + 15 < HEIGHT:  # down
             player.y += player_vel
         if keys[pygame.K_SPACE]:
             player.shoot()
@@ -276,7 +317,7 @@ def main(selected_weapon):
             enemy.move(enemy_vel)
             enemy.move_lasers(laser_vel, player)
 
-            if random.randrange(0, 2*60) == 1:
+            if random.randrange(0, 2 * 60) == 1:
                 enemy.shoot()
 
             if collide(enemy, player):
@@ -287,10 +328,17 @@ def main(selected_weapon):
                 enemies.remove(enemy)
 
         player.move_lasers(-laser_vel, enemies)
+
+
+weapon = "red"
+init = False
+
 def select_weapon(weapons, selected_weapon):
-    print("Selected: ", selected_weapon, "weapons: ", weapons)
-    print("You selected ",selected_weapon, "Demage: ", weapons.get(selected_weapon))
-    main(selected_weapon)
+    global weapon
+    weapon = selected_weapon
+    main()
+
+
 def main_menu():
     title_font = pygame.font.SysFont("comicsans", 70)
 
@@ -313,17 +361,14 @@ def main_menu():
         }
     }
 
-    menu_theme = pygame_menu.themes.THEME_DARK
-    menu = pygame_menu.Menu("Select a weapon", 300,300, theme=menu_theme)
     menu.disable()
     for weapon_name in weapons.keys():
-        print("weapon_name: ", weapon_name)
         menu.add.button(weapon_name, select_weapon, weapons, weapon_name)
 
     while run:
-        WIN.blit(BG, (0,0))
-        title_label = title_font.render("Press the mouse to begin...", 1, (255,255,255))
-        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+        WIN.blit(BG, (0, 0))
+        title_label = title_font.render("Press the mouse to begin...", 1, (255, 255, 255))
+        WIN.blit(title_label, (WIDTH / 2 - title_label.get_width() / 2, 350))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
